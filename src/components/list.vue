0eab9f939f5d0add4571e3aa0ce10469d2da7951
Chettawut <papae_peez02@hotmail.co.th>
2017-04-29 16:14:08 -0700

    success

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,6 +1,8 @@
 <template lang="html">
 <div class="container list-container">
-  <popup :flag="flagPopup" :detail="detailShop" @closePopup="closePopup"></popup>
+  <popup :flag="flagPopup" :cpop="cpop" :detail="detailShop" @closePopup="closePopup" :detailList="detailList"></popup>
+  <!-- <popup :flag="flagPopup" :cpop="cpop" :details="shops" :select-shop="selectShop" :latPop="latMap" :lngPop="lngMap" @closePopup="closePopup" ></popup> -->
+
   {{ loading(localhere[0]) }}
   <div class="columns is-tablet is-multiline list-fix">
     <div class="column is-one-third" v-for="(List, key, index) in Lists" @click="openPopup(List.reference)">
@@ -15,11 +17,12 @@
               </figure>
             </div>
             <div class="media-content">
-              <p class="title is-4">{{ List.name}}</p>
+              <p class="title is-4">{{ List.name }}</p>
             </div>
           </div>
           <div class="content">
-            ที่อยู่ : {{List.vicinity}}
+            ที่อยู่ : {{List.vicinity}}<br/>
+            ระยะทาง : {{ Number(HaversineInKM( List.geometry.location.lat , List.geometry.location.lng , localhere[0] , localhere[1]).toFixed(2)) }} กิโลเมตร
             <br>
             <small>11:09 PM - 1 Jan 2016</small>
           </div>
@@ -41,19 +44,24 @@ export default {
     return {
       Lists: '',
       num: 0,
+      detailList: true,
       flagPopup: false,
+      cpop: 'list',
       detailShop: {},
       localhere: []
     }
   },
   methods: {
     openPopup: function (reference) {
+      console.log('open')
       axios.get('https://maps.googleapis.com/maps/api/place/details/json?reference=' + reference + '&key=AIzaSyBar4Mz_GdBZW_ob2TK3rcffAB_rl5PnbE').then((response) => {
         this.detailShop = response.data.result
         this.flagPopup = true
+        console.log(this.detailShop)
       })
     },
     closePopup: function () {
+      console.log('close')
       this.flagPopup = false
     },
     getLocation: function () {
@@ -70,6 +78,15 @@ export default {
       axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + this.localhere[0] + '%2C' + this.localhere[1] + '&radius=5000&type=car&keyword=repair&key=AIzaSyDN0-75xfLIXbMfGDN--esKWkNpmYS3viw').then((response) => {
         this.Lists = response.data.results
       })
+    },
+    HaversineInKM: function (Lat, Long, LatHere, LongHere) {
+      var d2r = (Math.PI / 180)
+      var dlong = (LongHere - Long) * d2r
+      var dlat = (LatHere - Lat) * d2r
+      var a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(Lat * d2r) * Math.cos(LatHere * d2r) * Math.pow(Math.sin(dlong / 2), 2)
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+      var d = 6378.1370 * c
+      return d
     }
   },
   components: {
--- src/components/map.vue
+++ src/components/map.vue
@@ -5,10 +5,10 @@
         v-for="shop in shops"
         :position="shop.geometry.location"
         :clickable="true"
-        @click="TestClick(shop.geometry.location)"
+        @click="TestClick(shop.geometry.location, shop,shop.reference)"
       ></gmap-marker>
      </gmap-map>
-     <popup :flag="flagPopup" :detail="detailShop" :latPop="latMap" :lngPop="lngMap" @closePopup="closePopup" ></popup>
+     <popup :flag="flagPopup" :cpop="cpop" :details="shops" :select-shop="refShop" :latPop="latMap" :lngPop="lngMap" @closePopup="closePopup(locate)" ></popup>
   </div>
 </template>
 
@@ -29,27 +29,36 @@
     data () {
       return {
         center: {lat: 13.8220887, lng: 100.595177},
-        shops: [],
+        shops: {},
         flagPopup: false,
         detailShop: 0,
+        cpop: 'map',
         latMap: 0,
-        lngMap: 0
+        lngMap: 0,
+        selectShop: '',
+        refShop: {},
+        locate: ''
       }
     },
     beforeMount () {
       axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.8220887%2C100.595177&radius=5000&type=car&keyword=repair&key=AIzaSyDN0-75xfLIXbMfGDN--esKWkNpmYS3viw').then((response) => {
         this.shops = response.data.results
-        console.log(this.shops)
       })
     },
     methods: {
-      TestClick (position) {
+      TestClick (position, shop, locate) {
         this.center = position
         this.latMap = this.center.lat
         this.lngMap = this.center.lng
         this.flagPopup = true
+        this.selectShop = shop
+        this.locate = locate
       },
-      closePopup: function () {
+      closePopup: function (data) {
+        axios.get('https://maps.googleapis.com/maps/api/place/details/json?reference=' + data + '&key=AIzaSyBar4Mz_GdBZW_ob2TK3rcffAB_rl5PnbE').then((response) => {
+          this.refShop = response.data.result
+          console.log(this.refShop)
+        })
         this.flagPopup = false
       }
     },
